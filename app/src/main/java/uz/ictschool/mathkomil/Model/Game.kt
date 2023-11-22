450e54fe6313407285d1f6869f8b43ef5c67c8c1
komil06 <120444732+komil06@users.noreply.github.com>
2023-11-22 08:56:02 +0500

    Update Game.kt

--- app/src/main/java/uz/ictschool/mathkomil/Model/Game.kt
+++ app/src/main/java/uz/ictschool/mathkomil/Model/Game.kt
@@ -4,24 +4,29 @@ class Game {
     fun getRandom(mode: String): Pair<String, String> {
         var first = 0
         var second = 0
+        var correct_answer = 0
         when (mode) {
             "+" -> {
                 first = (1..10).random()
                 second = (1..10).random()
+                correct_answer = first + second
             }
 
             "-" -> {
                 first = (1..50).random()
                 second = (1..50).random()
+                correct_answer = first - second
             }
 
             "*" -> {
                 first = (1..100).random()
                 second = (1..100).random()
+                correct_answer = first * second
             }
             "/" -> {
                 first = (1..100).random()
                 second = (1..100).random()
+                correct_answer = first / second
             }
         }
     
@@ -35,7 +40,7 @@ class Game {
                 }
             }
         }
-        return Pair("$first $mode $second = ${solve(first, second, mode)}", mode)
+        return Pair("$first $mode $second = ${solve(first, second, mode)}", "$correct_answer")
     }
 
     private fun solve(first: Int, second: Int, mode: String): String {
