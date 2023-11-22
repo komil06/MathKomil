4c272b69f296a08b8e8544d36b7035bee7a73f70
komil06 <120444732+komil06@users.noreply.github.com>
2023-11-22 08:52:12 +0500

    Update Game.kt

--- app/src/main/java/uz/ictschool/mathkomil/Model/Game.kt
+++ app/src/main/java/uz/ictschool/mathkomil/Model/Game.kt
@@ -5,48 +5,41 @@ class Game {
         var first = 0
         var second = 0
         when (mode) {
-            "easy" -> {
+            "+" -> {
                 first = (1..10).random()
                 second = (1..10).random()
             }
 
-            "medium" -> {
+            "-" -> {
                 first = (1..50).random()
                 second = (1..50).random()
             }
 
-            "hard" -> {
+            "*" -> {
+                first = (1..100).random()
+                second = (1..100).random()
+            }
+            "/" -> {
                 first = (1..100).random()
                 second = (1..100).random()
             }
         }
-        val operators = listOf("*", "/", "+", "-")
-        val operator = operators.random()
-        if (operator == "/") {
+    
+        if (mode == "/") {
             while (first % second != 0) {
                 when (mode) {
-                    "easy" -> {
-                        first = (1..10).random()
-                        second = (1..10).random()
-                    }
-
-                    "medium" -> {
-                        first = (1..50).random()
-                        second = (1..50).random()
-                    }
-
-                    "hard" -> {
+                    "/" -> {
                         first = (1..100).random()
                         second = (1..100).random()
                     }
                 }
             }
         }
-        return Pair("$first _ $second = ${solve(first, second, operator)}", operator)
+        return Pair("$first $mode $second = ${solve(first, second, mode)}", mode)
     }
 
-    private fun solve(first: Int, second: Int, operator: String): String {
-        return when (operator) {
+    private fun solve(first: Int, second: Int, mode: String): String {
+        return when (mode) {
             "*" -> "${first * second}"
             "/" -> "${first / second}"
             "+" -> "${first + second}"
